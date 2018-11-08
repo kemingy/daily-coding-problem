1883c86dca6ba1f221185c43bb5cb8e402df5a25
keming <kemingy94@gmail.com>
2018-11-08 09:38:36 +0800

    fix lambda bug

---
+++ src/lambda_bug.py
@@ -0,0 +1,19 @@
+# What does the below code snippet print out? How can we fix the anonymous 
+# functions to behave as we'd expect?
+
+# ```
+# functions = []
+# for i in range(10):
+#     functions.append(lambda : i)
+
+# for f in functions:
+#     print(f())
+# ```
+
+
+functions = []
+for i in list(range(10)):
+    functions.append(lambda i=i: i)
+
+for f in functions:
+    print(f())
